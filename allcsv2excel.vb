e89c09e7b00af968b5c2a03945b94edf05fdee67
V Teles <47725556+V-Teles@users.noreply.github.com>
2022-06-14 13:50:47 +0200

    Create allcsv2excel.vb

---
+++ allcsv2excel.vb
@@ -0,0 +1,62 @@
+Option Explicit
+
+Sub Macro1()
+'
+' Macro1 Macro
+'
+    Dim strFile As String, strPath As String
+    
+
+    strPath = "C:\Users\belotelesvh\OneDrive - NTT Data EMEA\Desktop\DQ\"
+    strFile = Dir(strPath & "*.csv")
+    strPath = "TEXT;" & strPath
+
+'
+    While strFile <> ""
+
+        ActiveWorkbook.Sheets.Add After:=Worksheets(Worksheets.Count)
+        ActiveWorkbook.Sheets(Worksheets.Count).Name = Right(Left(strFile, InStr(strFile, ".csv") - 1), InStr(strFile, ".csv") - 7)
+        
+        With ActiveSheet.QueryTables.Add(Connection:=strPath & strFile, Destination:=Range("$A$1"))
+    '        .CommandType = 0
+            .Name = strFile
+            .FieldNames = True
+            .RowNumbers = False
+            .FillAdjacentFormulas = False
+            .PreserveFormatting = True
+            .RefreshOnFileOpen = False
+            .RefreshStyle = xlInsertDeleteCells
+            .SavePassword = False
+            .SaveData = True
+            .AdjustColumnWidth = True
+            .RefreshPeriod = 0
+            .TextFilePromptOnRefresh = False
+            .TextFilePlatform = 437
+            .TextFileStartRow = 1
+            .TextFileParseType = xlDelimited
+            .TextFileTextQualifier = xlTextQualifierDoubleQuote
+            .TextFileConsecutiveDelimiter = False
+            .TextFileTabDelimiter = False
+            .TextFileSemicolonDelimiter = False
+            .TextFileCommaDelimiter = False
+            .TextFileSpaceDelimiter = False
+            .TextFileOtherDelimiter = "|"
+            .TextFileColumnDataTypes = Array(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, _
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 _
+            , 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+            .TextFileTrailingMinusNumbers = True
+            .Refresh BackgroundQuery:=False
+            
+            
+        End With
+        Selection.AutoFilter
+        strFile = Dir
+    Wend
+
+End Sub
+
+
+
+
+
+
